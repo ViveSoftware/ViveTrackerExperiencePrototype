d9d11009daa8769b1657a1919232d833cb7b5473
chialun_ku <Chialun_Ku@htc.com>
2017-04-20 14:49:55 +0800

    Fix controllers/trackers not tracked for Unity 5.6 + SteamVR 1.2.1
    Attach script SteamVR_UpdatePoses to main camera if running Unity 5.6
    
    resolve #1

---
+++ Assets/Prototypes/Common/Scripts/SteamPoseUpdaterHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteamPoseUpdaterHelper : MonoBehaviour
+{
+    public GameObject mainCameraObject;
+
+	void Start ()
+    {
+#if UNITY_5_6
+        if (mainCameraObject != null)
+        {
+            mainCameraObject.AddComponent<SteamVR_UpdatePoses>();
+        }
+#endif
+    }
+}
